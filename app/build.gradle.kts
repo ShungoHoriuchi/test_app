5797265dacac94dba38e4866b3e607711f15a2e2
ShungoHoriuchi <hrshng4@gmail.com>
2025-04-30 23:30:45 +0900

    4/30 add button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
 
     defaultConfig {
         applicationId = "jp.ac.gifu_u.horiuchi.myapplication"
-        minSdk = 34
-        targetSdk = 34
+        minSdk = 35
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
