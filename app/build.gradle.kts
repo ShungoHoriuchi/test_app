82b17ee78bec1add8943ffa2006434bd8a5dde68
ShungoHoriuchi <hrshng4@gmail.com>
2025-05-21 11:45:56 +0900

    camera

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "jp.ac.gifu_u.horiuchi.myapplication"
-        minSdk = 35
+        minSdk = 29
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -37,7 +37,13 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.camera.core)
+    implementation("androidx.camera:camera-core:1.3.1")
+    implementation("androidx.camera:camera-camera2:1.3.1")
+    implementation("androidx.camera:camera-lifecycle:1.3.1")
+    implementation("androidx.camera:camera-view:1.3.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
--- app/src/main/java/jp/ac/gifu_u/horiuchi/myapplication/MainActivity.java
+++ app/src/main/java/jp/ac/gifu_u/horiuchi/myapplication/MainActivity.java
@@ -1,88 +1,169 @@
 package jp.ac.gifu_u.horiuchi.myapplication;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import android.Manifest;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 
-import androidx.activity.EdgeToEdge;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import java.util.List;
+import com.google.common.util.concurrent.ListenableFuture;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener , SensorEventListener {
+import java.util.concurrent.ExecutionException;
 
-    // センサを管理するマネージャ
-    private SensorManager manager;
+//public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        //setContentView(new MyView(this));
+//        //EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_main);
+////        Button b = (Button) findViewById(R.id.button);
+////        b.setOnClickListener(this);
+//
+////        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+////            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+////            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+////            return insets;
+////        });
+//    }
+//
+////    @Override
+////    public void onClick(View v) {
+////        showToast("アプリを終了しました");
+////        finish();
+////    }
+////
+////    private void showToast(String message) {
+////        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+////    }
+//
+////    public void onResume() {
+////        super.onResume();
+////        // 明るさセンサ(TYPE_LIGHT)のリストを取得
+////        List<Sensor> sensors =
+////                manager.getSensorList(Sensor.TYPE_LIGHT);
+////        // ひとつ以上見つかったら、最初のセンサを取得してリスナーに登録
+////        if (sensors.size() != 0) {
+////            Sensor sensor = sensors.get(0);
+////            manager.registerListener(
+////                    this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+////        }
+////    }
+////
+////    protected void onPause() {
+////        super.onPause();
+////        // 一時停止の際にリスナー登録を解除
+////        manager.unregisterListener(this);
+////    }
+////
+////    public void onAccuracyChanged(Sensor arg0, int arg1) {
+////    }
+////    public void onSensorChanged(SensorEvent arg0) {
+////        // 明るさセンサが変化したとき
+////        if (arg0.sensor.getType() == Sensor.TYPE_LIGHT) {
+////            // 明るさの値（単位ルクス）を取得
+////            float intensity = arg0.values[0];
+////            // 結果をテキストとして表示
+////            String str = Float.toString(intensity) + "ルクス";
+////            TextView textview =
+////                    (TextView) findViewById(R.id.status_text);
+////            textview.setText(str);
+////        }
+////    }
+//
+//}
+
+public class MainActivity extends AppCompatActivity{
+    private PreviewView previewView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(new MyView(this));
-//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-//        Button b = (Button) findViewById(R.id.button);
-//        b.setOnClickListener(this);
 
-        // マネージャを取得
-        manager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        previewView = findViewById(R.id.previewView);
 
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+        // パーミッションのチェック
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 100);
+        } else {
+            startCamera();
+        }
     }
 
     @Override
-    public void onClick(View v) {
-        showToast("アプリを終了しました");
-        finish();
-    }
-
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
-
-    public void onResume() {
-        super.onResume();
-// 明るさセンサ(TYPE_LIGHT)のリストを取得
-        List<Sensor> sensors =
-                manager.getSensorList(Sensor.TYPE_LIGHT);
-// ひとつ以上見つかったら、最初のセンサを取得してリスナーに登録
-        if (sensors.size() != 0) {
-            Sensor sensor = sensors.get(0);
-            manager.registerListener(
-                    this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCamera();
+            } else {
+                Toast.makeText(this, "カメラの許可が必要です", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
-    protected void onPause() {
-        super.onPause();
-// 一時停止の際にリスナー登録を解除
-        manager.unregisterListener(this);
+    private void startCamera() {
+        final ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    bindPreview(cameraProvider);
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, ContextCompat.getMainExecutor(this));
     }
 
-    public void onAccuracyChanged(Sensor arg0, int arg1) {
-    }
-    public void onSensorChanged(SensorEvent arg0) {
-// 明るさセンサが変化したとき
-        if (arg0.sensor.getType() == Sensor.TYPE_LIGHT) {
-// 明るさの値（単位ルクス）を取得
-            float intensity = arg0.values[0];
-// 結果をテキストとして表示
-            String str = Float.toString(intensity) + "ルクス";
-            TextView textview =
-                    (TextView) findViewById(R.id.status_text);
-            textview.setText(str);
-        }
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder().build();
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        cameraProvider.unbindAll();
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview);
     }
 
+    //@Override
+//    public void onClick(View v) {
+//        ImageCapture.OutputFileOptions outputFileOptions =
+//                new ImageCapture.OutputFileOptions.Builder(new File(...)).build();
+//        imageCapture.takePicture(outputFileOptions, cameraExecutor,
+//                new ImageCapture.OnImageSavedCallback() {
+//                    @Override
+//                    public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
+//                        // insert your code here.
+//                    }
+//                    @Override
+//                    public void onError(ImageCaptureException error) {
+//                        // insert your code here.
+//                    }
+//                }
+//        );
+//    }
 }
